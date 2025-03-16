0bf8c9e66cd336f7a6db7c7fba2ddaa0161490e8
Aldrich <aldrich07.a@gmail.com>
2025-03-16 21:23:02 +0800

    Changed code (Testing)

--- Test2.py
+++ Test2.py
@@ -1,19 +1,23 @@
 honda = 1000000
 toyota = 2000000
 aurelio = 3000000
-while True:
+checker = 1
+
+while checker == 1:
     try:
         print("WELCOME TO ABC DEALERSHIP")
         car = int(input("""    CHOOSE YOUR CAR
     1. Honda
     2. Toyota
     3. Aurelio
+    
 CHOSEN CAR: """))
 
         if car >= 1 and car <= 3:
             mop = int(input("""\n        CHOOSE MODE OF PAYMENT:
     1. Cash (10% Discount)
     2. Installment (5% Interest)
+    
 CHOSEN PAYMENT: """))
 
             try:
@@ -22,20 +26,17 @@ CHOSEN PAYMENT: """))
                         print("\n<=====RECEIPT=====>")
                         print("Chosen your car: Honda ")
                         print("Total amount: PHP", honda - (honda * 0.1))
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
                     elif car == 2:
                         print("\n<=====RECEIPT=====>")
                         print("Chosen your car: Toyota ")
                         print("Total amount: PHP", toyota - (toyota * 0.1))
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
                     elif car == 3:
                         print("\n<=====RECEIPT=====>")
                         print("Chosen your car: Aurelio ")
                         print("Total amount: PHP", aurelio - (aurelio * 0.1))
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
 
                 elif mop == 2:  # installment payment
                     raise Exception
@@ -48,28 +49,29 @@ CHOSEN PAYMENT: """))
                         print("Chosen your car: Honda ")
                         print("Total amount: PHP", honda + (honda * 0.05))
                         print("Monthly payment: PHP", (honda + (honda * 0.05)) / months)
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
                     elif car == 2:
                         print("\n<=====RECEIPT=====>")
                         print("Chosen your car: Toyota ")
                         print("Total amount: PHP", toyota + (toyota * 0.05))
                         print("Monthly payment: PHP", (toyota + (toyota * 0.05)) / months)
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
                     elif car == 3:
                         print("\n<=====RECEIPT=====>")
                         print("Chosen your car: Aurelio ")
                         print("Total amount: PHP", aurelio + (aurelio * 0.05))
                         print("Monthly payment: PHP", (aurelio + (aurelio * 0.05)) / months)
-                        print("<=====RECEIPT=====>")
-                        print(" ")
+                        print("<=====RECEIPT=====>\n")
                 else:
                     print("Invalid input")
         else:
             print("Invalid input")
+        checker = int(input("""Do you still want to continue?
+    1. Yes
+    2. No
+
+Enter here: """))
     except ValueError:
         print("Invalid input")
     finally:
-        print("Thank you for visiting ABC Dealership")
-        print(" ")
+        print("\nThank you for visiting ABC Dealership!!\n")
